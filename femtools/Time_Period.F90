cfa68052cf439838a37afecf459942962da67f13
Angus Gibson <angus@agibson.me>
2019-08-06 13:19:19 +1000

    Document time_period and reenable CPU and wall time dumps
    This should mimic the behaviour of the dumping performed in
    write_state, which may occur in terms of model time, timestep, CPU
    time or walltime.

--- femtools/Time_Period.F90
+++ femtools/Time_Period.F90
@@ -12,27 +12,39 @@ module time_period
 
   implicit none
 
+  !! Type for holding information about a previous dump period
   type time_period_type
     private
-    
+
+    !! whether the previous dump times have been initialised
     logical :: last_times_initialised = .false.
-    
+
+    !! the requested time between dumps in model seconds
     real :: real_dump_period = 0.0
-    integer :: int_dump_period = 0    
+    !! the requested time between dumps in timesteps
+    integer :: int_dump_period = 0
+    !! the last model time at which a dump was performed (or when this
+    !! control struct was initialised)
     real :: last_dump_time
-    
+
+    !! the last cpu time at which a dump was performed
     real :: last_dump_cpu_time
+    !! the last wall time at which a dump was performed
     real :: last_dump_wall_time
   end type time_period_type
 
 contains
   function should_output(CS, current_time, timestep, option_path)
+    !!< Given a time period control struct, should a new dump be
+    !!< created at this moment in time (considering model time, the
+    !!< current timestep, CPU time, and walltime)?
+
     logical :: should_output
 
     type(time_period_type), intent(inout) :: CS
     real, intent(in) :: current_time
     integer, intent(in) :: timestep
-    
+
     character(len=*), intent(in) :: option_path
 
     integer :: stat
@@ -50,7 +62,7 @@ contains
 
     if (have_option(trim(option_path) // "/dump_period")) then
       ! dump period isn't set, or dump period has elapsed since the last time there was a dump
-      
+
       if (CS%real_dump_period == 0.0 .or. dump_count_greater(current_time, CS%last_dump_time, CS%real_dump_period)) then
         if (have_option(trim(option_path) // "/dump_period/constant")) then
           call get_option(trim(option_path) // "/dump_period/constant", CS%real_dump_period)
@@ -94,31 +106,28 @@ contains
 
     if (.not. have_option(trim(option_path) // "/dump_period") .and. .not. have_option(trim(option_path) // "/dump_period_in_timesteps")) then
       ! if the option isn't set, assume always output
-      
+
       should_output = .true.
       return
     end if
 
-    ! disabled for the moment
-    if (.false.) then
-      call cpu_time(current_cpu_time)
-      call allmax(current_cpu_time)
-      call get_option(trim(option_path) // "/cpu_dump_period", CS%real_dump_period, stat)
-      if (stat == SPUD_NO_ERROR) then
-        if (CS%real_dump_period == 0.0 .or. dump_count_greater(current_cpu_time, CS%last_dump_cpu_time, CS%real_dump_period)) then
-          should_output = .true.
-          return
-        end if
+    call cpu_time(current_cpu_time)
+    call allmax(current_cpu_time)
+    call get_option(trim(option_path) // "/cpu_dump_period", CS%real_dump_period, stat)
+    if (stat == SPUD_NO_ERROR) then
+      if (CS%real_dump_period == 0.0 .or. dump_count_greater(current_cpu_time, CS%last_dump_cpu_time, CS%real_dump_period)) then
+        should_output = .true.
+        return
       end if
+    end if
 
-      current_wall_time = wall_time()
-      call allmax(current_wall_time)
-      call get_option(trim(option_path) // "/wall_time_dump_period", CS%real_dump_period, stat)
-      if (stat == SPUD_NO_ERROR) then
-        if (CS%real_dump_period == 0.0 .or. dump_count_greater(current_wall_time, CS%last_dump_wall_time, CS%real_dump_period)) then
-          should_output = .true.
-          return
-        end if
+    current_wall_time = wall_time()
+    call allmax(current_wall_time)
+    call get_option(trim(option_path) // "/wall_time_dump_period", CS%real_dump_period, stat)
+    if (stat == SPUD_NO_ERROR) then
+      if (CS%real_dump_period == 0.0 .or. dump_count_greater(current_wall_time, CS%last_dump_wall_time, CS%real_dump_period)) then
+        should_output = .true.
+        return
       end if
     end if
 
@@ -136,8 +145,11 @@ contains
   end function should_output
 
   subroutine init_output_CS(CS, option_path)
+    !!< Initialise a time period control structure, reading options
+    !!< from the specified option_path.
+
     type(time_period_type), intent(inout) :: CS
-    character(len=*), intent(in) :: option_path
+    character(len=*), intent(in) :: option_path !! path prefix for reading options
 
     character(len=OPTION_PATH_LEN) :: func
 
@@ -145,7 +157,7 @@ contains
 
     if (have_option(trim(option_path) // "/dump_period/constant")) then
       call get_option(trim(option_path) // "/dump_period/constant", CS%real_dump_period)
-      
+
     else if (have_option(trim(option_path) // "/dump_period/python")) then
       ! set a dummy dump period and call to the set function -- we don't count this as
       ! initialising the last dump time (unlike update_output_CS)
@@ -159,7 +171,7 @@ contains
 
     else if (have_option(trim(option_path) // "/dump_period_in_timesteps/constant")) then
       call get_option(trim(option_path) // "/dump_period_in_timesteps/constant", CS%int_dump_period)
-      
+
     else if (have_option(trim(option_path) // "/dump_period_in_timesteps/python")) then
       ! set a dummy period and call the function
       CS%int_dump_period = huge(0)
@@ -173,10 +185,21 @@ contains
   end subroutine init_output_CS
 
   subroutine update_output_CS(CS, current_time)
+    !!< Update the given time period control structure after
+    !!< a dump has been made.
+
     type(time_period_type), intent(inout) :: CS
     real, intent(in) :: current_time
 
     CS%last_times_initialised = .true.
+
+    ! update model, cpu and walltimes in the structure
     CS%last_dump_time = current_time
+
+    call cpu_time(CS%last_dump_cpu_time)
+    call allmax(CS%last_dump_cpu_time)
+
+    CS%last_dump_wall_time = wall_time()
+    call allmax(CS%last_dump_wall_time)
   end subroutine update_output_CS
 end module time_period
