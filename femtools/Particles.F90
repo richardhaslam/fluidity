7be5aae5088c9fd9a3f5d1023941ff4f502bd16d
Angus Gibson <angus@agibson.me>
2019-09-10 13:58:40 +1000

    Revert "Remove allgather in particle checkpointing"
    This reverts commit e2e203b9648242f60b24c16a0fea2935a1e00854.

--- femtools/Particles.F90
+++ femtools/Particles.F90
@@ -1462,13 +1462,12 @@ contains
     if (present(output_comm)) comm = output_comm
 
     ! we store the number of points per process, so gather them
-    ! onto the root (which is responsible for writing the
-    ! attribute to disk) -- this works as long as we don't later
-    ! read the npoints attribute
+    ! this is because h5part attributes must be agreed upon by all,
+    ! so every process needs to know the size for every other process
     call mpi_comm_size(comm, commsize, ierr)
     allocate(npoints(commsize))
-    call mpi_gather([int(particle_list%length, 8)], 1, MPI_INTEGER8, &
-         npoints, 1, MPI_INTEGER8, 0, comm, ierr)
+    call mpi_allgather([int(particle_list%length, 8)], 1, MPI_INTEGER8, &
+         npoints, 1, MPI_INTEGER8, comm, ierr)
 
     ! construct a new particle checkpoint filename
     particles_cp_filename = trim(prefix)
